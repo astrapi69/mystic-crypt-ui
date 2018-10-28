6bf55160d45fb21588c87d161c9bb24b2a90382a
Asterios Raptis <asterios.raptis@gmx.net>
2018-10-28 11:52:50 +0100

    tagged ModeContext as deprecated

--- src/main/java/de/alpharogroup/mystic/crypt/panels/obfuscate/ModeContext.java
+++ src/main/java/de/alpharogroup/mystic/crypt/panels/obfuscate/ModeContext.java
@@ -3,42 +3,42 @@
  *
  * Copyright (C) 2015 Asterios Raptis
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package de.alpharogroup.mystic.crypt.panels.obfuscate;
 
 /**
- * The enum {@link ModeContext} that define the data manipulation process
+ * The enum {@link ModeContext} provides modification modes for create, delete, update and no
+ * modification
+ * 
+ * @deprecated use instead the same name enum class in the project model-core when the next release
+ *             is out
  */
 public enum ModeContext
 {
 
-	/** The create mode. */
+	/** The modification mode that signals the creation mode */
 	CREATE,
 
-	/** The delete mode. */
+	/** The modification mode that signals the deletion mode */
 	DELETE,
 
-	/** The none mode. */
+	/** The mode that signals that no modification will be done */
 	NONE,
 
-	/** The update mode. */
+	/** The modification mode that signals the update mode */
 	UPDATE
 }
